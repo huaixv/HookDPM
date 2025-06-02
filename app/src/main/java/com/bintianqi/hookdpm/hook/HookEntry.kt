7c420b9ffdd29a910e2367f75d8052bef0b0c08e
huanli233 <392352840@qq.com>
2025-06-02 22:18:59 +0800

    feat: real-time config reading of ForceDeviceOwner and ForceProfileOwner
    Move config reading to the "before" block of the hook, so that the config is read every time the method is called.

--- app/src/main/java/com/bintianqi/hookdpm/hook/HookEntry.kt
+++ app/src/main/java/com/bintianqi/hookdpm/hook/HookEntry.kt
@@ -13,88 +13,93 @@ import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 object HookEntry : IYukiHookXposedInit {
     override fun onHook() = encase {
         loadSystem {
-            val dpms = "com.android.server.devicepolicy.DevicePolicyManagerService".toClass()
-            dpms.method {
-                name = "enforceCanSetDeviceOwnerLocked"
-                paramCount = 4
-                returnType = UnitType
-            }.hook {
-                if(prefs.getBoolean("force_do", false)) {
-                    replaceUnit {  }
-                }
-            }
-            dpms.method {
-                name = "checkDeviceOwnerProvisioningPreConditionLocked"
-                paramCount = 5
-                returnType = IntType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("force_do", false)) {
-                        result = 0
+            "com.android.server.devicepolicy.DevicePolicyManagerService".toClass().apply {
+                method {
+                    name = "enforceCanSetDeviceOwnerLocked"
+                    paramCount = 4
+                    returnType = UnitType
+                }.hook {
+                    before {
+                        if (prefs.getBoolean("force_do", false)) {
+                            resultNull()
+                        }
                     }
                 }
-            }
-            dpms.method {
-                name = "enforceCanSetProfileOwnerLocked"
-                paramCount = 4
-                returnType = UnitType
-            }.hook {
-                if(prefs.getBoolean("force_po", false)) {
-                    replaceUnit { }
-                }
-            }
-            dpms.method {
-                name = "hasIncompatibleAccountsOnAnyUser"
-                emptyParam()
-                returnType = BooleanType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("bypass_account_check", false)) {
-                        result = false
+                method {
+                    name = "checkDeviceOwnerProvisioningPreConditionLocked"
+                    paramCount = 5
+                    returnType = IntType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("force_do", false)) {
+                            result = 0
+                        }
                     }
                 }
-            }
-            dpms.method {
-                name = "hasAccountsOnAnyUser"
-                emptyParam()
-                returnType = BooleanType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("bypass_account_check", false)) {
-                        result = false
+                method {
+                    name = "enforceCanSetProfileOwnerLocked"
+                    paramCount = 4
+                    returnType = UnitType
+                }.hook {
+                    before {
+                        if (prefs.getBoolean("force_po", false)) {
+                            resultNull()
+                        }
                     }
                 }
-            }
-            dpms.method {
-                name = "nonTestNonPrecreatedUsersExist"
-                emptyParam()
-                returnType = BooleanType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("enhanced_mode", false)) {
-                        result = false
+                method {
+                    name = "hasIncompatibleAccountsOnAnyUser"
+                    emptyParam()
+                    returnType = BooleanType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("bypass_account_check", false)) {
+                            result = false
+                        }
                     }
                 }
-            }
-            dpms.method {
-                name = "isProvisioningAllowed"
-                param(StringClass, StringClass)
-                returnType = BooleanType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("hook_ipa", false)) {
-                        result = true
+                method {
+                    name = "hasAccountsOnAnyUser"
+                    emptyParam()
+                    returnType = BooleanType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("bypass_account_check", false)) {
+                            result = false
+                        }
                     }
                 }
-            }
-            dpms.method {
-                name = "checkProvisioningPrecondition"
-                param(StringClass, StringClass)
-                returnType = IntType
-            }.hook {
-                after {
-                    if(prefs.getBoolean("hook_cpp", false)) {
-                        result = 0
+                method {
+                    name = "nonTestNonPrecreatedUsersExist"
+                    emptyParam()
+                    returnType = BooleanType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("enhanced_mode", false)) {
+                            result = false
+                        }
+                    }
+                }
+                method {
+                    name = "isProvisioningAllowed"
+                    param(StringClass, StringClass)
+                    returnType = BooleanType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("hook_ipa", false)) {
+                            result = true
+                        }
+                    }
+                }
+                method {
+                    name = "checkProvisioningPrecondition"
+                    param(StringClass, StringClass)
+                    returnType = IntType
+                }.hook {
+                    after {
+                        if (prefs.getBoolean("hook_cpp", false)) {
+                            result = 0
+                        }
                     }
                 }
             }
